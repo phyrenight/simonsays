b06b3960ffe96a16fcff58ebe06e3b71e6ee7486
Marc <phyrenight@gmail.com>
2017-01-13 23:04:33 -0500

    functionality: add function to start game

--- js/JSscript.js
+++ js/JSscript.js
@@ -1,18 +1,32 @@
 (function(){
+  var gameState = {
+    gameEnd : 'false',
+    randomSequence : [],
+    userSequence : [],
+    stage : 0,
+    onOff : false
+  };
+
   var green = document.getElementById('green');
   var red = document.getElementById('red');
   var yellow = document.getElementById('yellow');
   var blue = document.getElementById('blue');
+  var onOff = document.getElementById('onOff');
 
   blue.addEventListener('mousedown', lightUpColor, false);
   blue.addEventListener('mouseup', backToNormal, false);
+  
   red.addEventListener('mousedown', lightUpColor, false);
   red.addEventListener('mouseup', backToNormal, false);
+  
   yellow.addEventListener('mousedown', lightUpColor, false);
-  yellow.addEventListener('mouseup', backToNormal, false)
+  yellow.addEventListener('mouseup', backToNormal, false);
+  
   green.addEventListener('mousedown', lightUpColor, false);
   green.addEventListener('mouseup', backToNormal, false);
   
+  onOff.addEventListener('click', startGame, false);
+
   function backToNormal(){
     /*
       changes the button color back to normal
@@ -48,26 +62,34 @@
       blue.style.backgroundColor = "#0000FF";
     }
   }
-})();
 
-var sequence = [];
-
-var start_game = function(){
-  var i = 0;
-  sequence = [];
-  while(i < 4){
-    sequence.push(random(0, 4));
-    i++;
+  function startGame(){
+    var i = 0;
+    if(!gameState.onOff){
+      while(i < 4){
+        gameState.randomSequence.push(random(0,4));
+        i++;
+        gameState.onOff = true;
+      }
+    }
+    else{
+      gameState.onOff = false;
+    }
   }
-  console.log(sequence);
-  activate_sequence(sequence);
-} 
 
-var random = function(min, max){
-  number = Math.floor(Math.random() * (max - min)) + 
-    min;
-  return number;
-}
+  var random = function(min, max){
+    var number = Math.floor(Math.random() * (max - min)) + 
+      min;
+    return number;
+  };
+
+  function checkUserInput(){
+    var numberOfUserInputs = gameState.UserSequence.length
+    console.log(numberOfUserInputs);
+
+  }
+
+})();
 
 var activate_sequence = function(sequence){
   var color = "";
@@ -77,9 +99,9 @@ var activate_sequence = function(sequence){
       if(sequence[i] === 0){
         //green
         color = "green";
-        newcolor = "";
+        newColor = "";
         lightUpButton(color, newColor);
-        sleep()
+        sleep();
       }
       else if(sequence[i] == 1){
         // red
@@ -100,14 +122,14 @@ var activate_sequence = function(sequence){
         lightUpButton(color, newColor);
       }
       else{
-      	console.log("Error: Invalid number in activate_sequence")
+      	console.log("Error: Invalid number in activate_sequence");
       }
     }
   }
   else{
-  	start_game()
+  	start_game();
   }
-}
+};
 
 var lightUpButton = function(color, newColor){
   /*
@@ -117,11 +139,11 @@ var lightUpButton = function(color, newColor){
   */
   var div = document.getElementById(color);
   var divOriginalColor = div.style.backgroundColor;
-  div.style.backgroundColor = "pink"
+  div.style.backgroundColor = "pink";
   //setTimeout(function(){div.style.backgroundColor = divOriginalColor}, 3000) 
   //sleep();
   //div.style.backgroundColor =  divOriginalColor;
-}
+};
 
 
 var sleep = function(){
@@ -132,8 +154,8 @@ var sleep = function(){
   var longNumber = 1000000000000000000;
   for(var i = 0; i < longNumber; i++){
   	if((new Date().getTime() - start) > milliseconds){
-  		console.log(new Date().getTime() - start)
+  		console.log(new Date().getTime() - start);
   		break;
   	}
   }
-}
+};
